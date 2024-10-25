ec7ce42edc0a5080697fab8355bbf1984690e6d4
Oleg Bedrin <numeralhuman@gmail.com>
2024-10-25 19:51:50 +0400

    testing rule 30

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,16 +1,13 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.15;
 
-import "foundry-huff/HuffDeployer.sol";
-import "forge-std/Script.sol";
+import { HuffDeployer } from "foundry-huff/HuffDeployer.sol";
+import { Script } from "forge-std/Script.sol";
 
-interface SimpleStore {
-  function setValue(uint256) external;
-  function getValue() external returns (uint256);
-}
+import { Role30 } from "./huffInterfaces/Rule30.sol";
 
 contract Deploy is Script {
-  function run() public returns (SimpleStore simpleStore) {
-    simpleStore = SimpleStore(HuffDeployer.deploy("SimpleStore"));
+  function run() public returns (Rule30 rule30) {
+    rule30 = Role30(HuffDeployer.deploy("Rule30"));
   }
 }
---
+++ test/Rule30.test.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.15;
+
+import { HuffDeployer } from "foundry-huff/HuffDeployer.sol";
+import { Test } from "forge-std/Test.sol";
+import { console } from "forge-std/console.sol";
+
+import { Role30 } from "../script/huffInterfaces/Rule30.sol";
+
+contract Rule30Test is Test {
+    /// @dev Address of the Rule30 contract.
+    Rule30 public rule30;
+
+    /// @dev Setup the testing environment.
+    function setUp() public {
+        rule30 = Rule30(HuffDeployer.deploy("Rule30"));
+    }
+
+    /// @dev Ensure that you can success test the cellular automata
+    function testRule30() public {
+        rule30.initState();
+        rule30.iterateCells();
+    }
+}
